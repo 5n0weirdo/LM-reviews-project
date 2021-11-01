fa953aee3a29265056db589c35636a59bd901867
5n0weirdo <snoweirdo@protonmail.com>
2021-11-01 13:24:39 +0200

    Display an underline

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Review from "./Review";
 function App() {
   return (
     <main>
-      <div className="container">
+      <section className="container">
         <div className="title">
           <h2>Our Reviews</h2>
+          <div className="underline"></div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
